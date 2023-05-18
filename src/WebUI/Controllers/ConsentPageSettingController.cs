37034860c2a5a30a4f66a6ecc6bef00571d1dffb
Peerapat Kaosoon <68856496+PRP-4979@users.noreply.github.com>
2023-05-18 14:48:30 +0700

    Get Consent Themes

--- src/WebUI/Controllers/ConsentPageSettingController.cs
+++ src/WebUI/Controllers/ConsentPageSettingController.cs
@@ -1,14 +1,18 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Wisework.ConsentManagementSystem.Api;
+using WW.Application.Common.Models;
+using WW.Application.ConsentPageSetting.Queries.GetConsentTheme;
+using WW.Domain.Entities;
 using WW.OpenAPI.Controllers;
 
 namespace Wiskwork.OpenAPI.Controllers
 {
     public class ConsentPageSettingController : ApiControllerBase
     {
-        /*[HttpGet]
-        public async Task<ActionResult<GeneralConsent>> GetGeneralConsentInfoQuery(GeneralConsentInfoRequestQuery query)
+        [HttpGet]
+        public async Task<ActionResult<PaginatedList<ConsentTheme>>> GetConsentThemeQuery([FromQuery] GetConsentThemeQuery query)
         {
             return await Mediator.Send(query);
-        }*/
+        }
     }
 }
