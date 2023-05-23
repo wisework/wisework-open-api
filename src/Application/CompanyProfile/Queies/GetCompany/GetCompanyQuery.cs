46619fa5cea359a95536e0925419c4b7e553e1f3
Panyawut Suton <92622255+BossPanyawut@users.noreply.github.com>
2023-05-23 08:38:33 +0700

    fix: Implement rest API creating companies

--- src/Application/CompanyProfile/Queies/GetCompany/GetCompanyQuery.cs
+++ src/Application/CompanyProfile/Queies/GetCompany/GetCompanyQuery.cs
@@ -1,18 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using AutoMapper;
 using AutoMapper.QueryableExtensions;
 using MediatR;
+using Wisework.ConsentManagementSystem.Api;
 using WW.Application.Common.Interfaces;
 using WW.Domain.Entities;
+using WW.Domain.Enums;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
 
 namespace WW.Application.CompanyProfile.Queies.GetCompany;
-public record GetCompanyQuery : IRequest<Company>;
+public record GetCompanyQuery(int id) : IRequest<Company>;
 
-public class GetCompanyQueryHandler : RequestHandler<GetCompanyQuery, Company>
+public class GetCompanyQueryHandler : IRequestHandler<GetCompanyQuery, Company>
 {
     private readonly IApplicationDbContext _context;
     private readonly IMapper _mapper;
@@ -23,14 +27,33 @@ public class GetCompanyQueryHandler : RequestHandler<GetCompanyQuery, Company>
         _mapper = mapper;
     }
 
-    protected override Company Handle(GetCompanyQuery request)
+    public async Task<Company> Handle(GetCompanyQuery request, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
-    }
+        MapperConfiguration config = new MapperConfiguration(cfg =>
+        {
+            cfg.CreateMap<Companies, Company>();
+        });
 
-    /*protected override Company Handle(GetCompanyQuery request)
-    {
-        // todo: get company id from token
-       // return (Company)_context.DbSetCompanies.Select(company => company.CompanyId == 1).ProjectTo<Company>(_mapper.ConfigurationProvider);
-    }*/
+        Mapper mapper = new Mapper(config);
+
+        var companyInfo = (from cf in _context.DbSetCompanies
+                               where cf.CompanyId == request.id && cf.CompanyId == 1 && cf.Status != Status.X.ToString()
+                               select new Company
+                               {
+                                  CompanyId = cf.CompanyId,
+                                   CompanyName =cf.Name,
+                                   LogoImage = cf.LogoImage,
+                                   Status = cf.Status,
+                                   CreateBy = cf.CreateBy.ToString(),
+                                   CreateDate= cf.CreateDate.ToString(),
+                                   UpdateBy= cf.UpdateBy.ToString(),
+                                   UpdateDate= cf.UpdateDate.ToString(),
+                               }).FirstOrDefault();
+
+        if (companyInfo == null)
+        {
+            return new Company();
+        }
+        return companyInfo;
+    }
 }
---
+++ src/WebUI/Controllers/CompanyController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Wisework.ConsentManagementSystem.Api;
+using WW.Application.CompanyProfile.Queies.GetCompany;
+using WW.Application.CustomField.Queries.GetCustomField;
+using WW.OpenAPI.Controllers;
+
+namespace Wiskwork.OpenAPI.Controllers;
+
+public class CompanyController : ApiControllerBase
+{
+    [HttpGet("{id}")]
+    public async Task<Company> Get(int id)
+    {
+        return await Mediator.Send(new GetCompanyQuery(id));
+    }
+}
