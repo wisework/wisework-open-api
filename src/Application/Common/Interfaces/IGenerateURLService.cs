1d1d324599d062d89fcbf3ad36ab1e6e23d67f4e
Peerapat Kaosoon <68856496+PRP-4979@users.noreply.github.com>
2023-05-31 14:14:57 +0700

    Add interface, model and service of Generate URL

---
+++ src/Application/Common/Interfaces/IGenerateURLService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WW.Application.Common.Models;
+
+namespace WW.Application.Common.Interfaces;
+public interface IGenerateURLService
+{
+    Task<GenerateURLModel> GenerateShortenURL(GenerateURLModel url);
+}
+
