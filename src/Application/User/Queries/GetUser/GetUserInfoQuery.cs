549f0d45a64b98eeb98609baf2fcdc64b5a312d1
Panyawut Suton <92622255+BossPanyawut@users.noreply.github.com>
2023-06-20 14:19:31 +0700

    apply handle error code 500, 404 and 400

--- src/Application/User/Queries/GetUser/GetUserInfoQuery.cs
+++ src/Application/User/Queries/GetUser/GetUserInfoQuery.cs
@@ -3,9 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 using AutoMapper;
+using FluentValidation.Results;
 using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using Wisework.ConsentManagementSystem.Api;
+using WW.Application.Common.Exceptions;
 using WW.Application.Common.Interfaces;
 using WW.Domain.Entities;
 using WW.Domain.Enums;
@@ -24,46 +30,78 @@ public class GetUserInfoQueryHandler : IRequestHandler<GetUserInfoQuery, UserInf
         _mapper = mapper;
     }
 
-    
-
     public async Task<UserInfo> Handle(GetUserInfoQuery request, CancellationToken cancellationToken)
     {
-        MapperConfiguration config = new MapperConfiguration(cfg =>
+        try
         {
-            cfg.CreateMap<Users, UserInfo>();
-            cfg.CreateMap<string, Guid?>().ConvertUsing(s => String.IsNullOrWhiteSpace(s) ? (Guid?)null : Guid.Parse(s));
-        });
+            MapperConfiguration config = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<Users, UserInfo>();
+                cfg.CreateMap<string, Guid?>().ConvertUsing(s => String.IsNullOrWhiteSpace(s) ? (Guid?)null : Guid.Parse(s));
+            });
 
-        Mapper mapper = new Mapper(config);
+            Mapper mapper = new Mapper(config);
 
-        var userInfo = (from cf in _context.DbSetUser
-                        where cf.UserId == request.id && cf.Status != Status.X.ToString()
-                        select new UserInfo
-                        {
-                            Address = cf.Address,
-                            BirthDate = cf.BirthDate.ToString(),
-                            CitizenId = cf.CitizenId,
-                            //CompanyId = cf.CompanyId,
-                            CompanyId = 1,
-                            Email = cf.Email,
-                            FullName = $"{cf.FirstName} {cf.LastName}",
-                            Guid = new Guid(cf.Guid),
-                            Gender = cf.Gender,
-                            //PositionDescription = cf.PositionDescription,
-                            PositionDescription = "IT",
-                            //ProfileImage = cf.ProfileImage,
-                            ProfileImage = "string",
-                            Tel = cf.Tel,
-                            UserID = Convert.ToInt32(cf.UserId),
-                            Username = cf.Username,
-                            Version = cf.Version,
-                        }).FirstOrDefault();
+            var userInfo = (from cf in _context.DbSetUser
+                            where cf.UserId == request.id && cf.Status != Status.X.ToString()
+                            select new UserInfo
+                            {
+                                Address = cf.Address,
+                                BirthDate = cf.BirthDate.ToString(),
+                                CitizenId = cf.CitizenId,
+                                //CompanyId = cf.CompanyId,
+                                CompanyId = 1,
+                                Email = cf.Email,
+                                FullName = $"{cf.FirstName} {cf.LastName}",
+                                Guid = new Guid(cf.Guid),
+                                Gender = cf.Gender,
+                                //PositionDescription = cf.PositionDescription,
+                                PositionDescription = "IT",
+                                //ProfileImage = cf.ProfileImage,
+                                ProfileImage = "string",
+                                Tel = cf.Tel,
+                                UserID = Convert.ToInt32(cf.UserId),
+                                Username = cf.Username,
+                                Version = cf.Version,
+                            }).FirstOrDefault();
 
-        if (userInfo == null)
+            if (request.id < 0)
+            {
+                List<ValidationFailure> failures = new List<ValidationFailure> { };
+
+                failures.Add(new ValidationFailure("ID", "ID must be greater than 0"));
+
+                throw new ValidationException(failures);
+            }
+            else if (userInfo == null)
+            {
+                throw new NotFoundException("User not found"); // 404 Not Found
+            }
+
+            return userInfo;
+        }
+        catch (NotFoundException)
         {
-            return new UserInfo();
+            throw new NotFoundException("User not found"); // 404 Not Found
+        }
+        catch (ValidationException)
+        {
+            List<ValidationFailure> failures = new List<ValidationFailure> { };
+
+            failures.Add(new ValidationFailure("ID", "ID must be greater than 0"));
+
+            throw new ValidationException(failures);
         }
 
-        return userInfo;
+        catch (Exception ex)
+        {
+            throw new InternalException("An internal server error occurred."); // 500 error
+        }
     }
+
+
+
 }
+
+
+
