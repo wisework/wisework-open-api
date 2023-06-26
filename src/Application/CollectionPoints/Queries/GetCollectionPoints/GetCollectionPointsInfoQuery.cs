cbcf1d058c6fe1cc949f97db7dccd2271bf96bbd
Gun Jiraphat <sriyuyongj@gmail.com>
2023-06-26 22:09:33 +0700

    fix : Dll v.17 & Infoquery & query

--- src/Application/CollectionPoints/Queries/GetCollectionPoints/GetCollectionPointsInfoQuery.cs
+++ src/Application/CollectionPoints/Queries/GetCollectionPoints/GetCollectionPointsInfoQuery.cs
@@ -33,136 +33,118 @@ public class GetCollectionPointsInfoQueryHandler : IRequestHandler<GetCollection
         MapperConfiguration config = new MapperConfiguration(cfg =>
         {
             cfg.CreateMap<Consent_CollectionPoint, CollectionPointInfo>();
+            cfg.CreateMap<string, Guid?>().ConvertUsing(s => String.IsNullOrWhiteSpace(s) ? (Guid?)null : Guid.Parse(s));
+
         });
 
         Mapper mapper = new Mapper(config);
 
         //todo:edit conpanyid หลังมีการทำ identity server
         var collectionpointInfo = (from cp in _context.DbSetConsentCollectionPoints
-                                   join uCreate in _context.DbSetUser on cp.CreateBy equals uCreate.CreateBy
-                                   join uUpdate in _context.DbSetUser on cp.CreateBy equals uUpdate.UpdateBy
-                                   join c in _context.DbSetCompanies on cp.CompanyId equals (int)(long)c.CompanyId
-                                   join w in _context.DbSetConsentWebsite on (int)(long)c.CompanyId equals w.CompanyId
+                                   
                                    where cp.CollectionPointId == request.Id && cp.CompanyId == 1 && cp.Status != Status.X.ToString()
                                    select new CollectionPointInfo
                                    {
                                        CollectionPointId = cp.CollectionPointId,
-                                       /*CollectionPoint = cp.CollectionPoint,
-                                       WebsiteId = cp.WebsiteId,
-                                       AccessToken = c.AccessToken,
-                                       Guid = cp.Guid,
-                                       WebsiteDescription = w.Description,
-                                       WebsiteUrl = w.Url,
-                                       WebsitePolicy = w.Urlpolicy,
-                                       Description = cp.Description,
-                                       Script = cp.Script,*/
-                                       CompanyId = (int)(long)c.CompanyId,
-                                       Version = c.Version,
-                                       Status = c.Status,
-                                       /*StatusDisplay = c.Status == "Active" ? Status.Active.ToString() :Status.Inactive.ToString(),
-                                       CreateBy = c.CreateBy,
-                                       CreateByDisplay = String.Format("{0} {1}", uCreate.FirstName, uCreate.LastName),
-                                       CreateDate = c.CreateDate.ToLocalTime(),
-                                       CreateDateDisplay = c.CreateDate.LocalDateTime.ToShortDateString(),
-                                       UpdateBy = c.UpdateBy,
-                                       UpdateByDisplay = String.Format("{0} {1}", uUpdate.FirstName, uUpdate.LastName),
-                                       IsStatus = c.Status == "Active" ? true : false*/
+                                       CollectionPointName = cp.CollectionPoint,
+                                       Guid = new Guid(cp.Guid),
+                                       ExpiredDateTime = cp.KeepAliveData,
+                                       CompanyId = (int)(long)cp.CompanyId,
+                                       Version = cp.Version,
+                                       Status = cp.Status,
+         
                                    }).FirstOrDefault();
 
+        #region website
+        var websiteList = (from cp in _context.DbSetConsentCollectionPoints
+                           join w in _context.DbSetConsentWebsite on cp.WebsiteId equals w.WebsiteId
+                           where cp.CollectionPointId == request.Id
+                           select
+                               new CollectionPointInfoWebsiteObject
+                               {
+                                   Id = w.WebsiteId,
+                                   Description = w.Description,
+                                   UrlHomePage = w.Url,
+                                   UrlPolicyPage = w.Urlpolicy,
+                               }).FirstOrDefault();
+
         
+        #endregion
+
         #region purpose list
         var purposeList = (from cp in _context.DbSetConsentCollectionPointItem
                            join p in _context.DbSetConsentPurpose on cp.PurposeId equals p.PurposeId
                            where cp.Status == Status.Active.ToString()
                            && p.Status == Status.Active.ToString()
                            && cp.CollectionPointId == request.Id
-                           select
+                           select 
                                new GeneralConsentPurpose
                                {
                                    PurposeId = p.PurposeId,
-                                   //PurposeType = p.PurposeType == 1 ? PurposeType.Consent.ToString() : PurposeType.Cookie.ToString(),
+                                   CompanyId = p.CompanyId,
                                    Code = p.Code,
                                    Description = p.Description,
                                    WarningDescription = p.WarningDescription,
                                    PurposeCategoryId = p.PurposeCategoryId,
                                    ExpiredDateTime = Calulate.ExpiredDateTime(p.KeepAliveData, p.CreateDate),
-                                   //Guid = p.Guid,
+                                   Guid = new Guid(p.Guid),
                                    Version = p.Version,
                                    Priority = cp.Priority,
-                                   Status = p.Status,
-                                   CompanyId = p.CompanyId,
-                                   /*CreateBy = p.CreateBy,
-                                   CreateDate = p.CreateDate.ToLocalTime(),
-                                   UpdateBy = p.UpdateBy,
-                                   UpdateDate = p.UpdateDate.ToLocalTime(),
-                                   Active = p.Status == Status.Active.ToString()?true:false*/
-
+                                   Status = p.Status
                                }).ToList();
 
-        
         #endregion
 
         #region custom fields
-
-        var customFieldsList = (from cf in _context.DbSetConsent_CollectionPointCustomFieldConfig
-
-                                //where collectionpointInfo.Guid == cf.CollectionPointGuid
-                                select 
-                                    new CollectionPointCustomFields
-                                    {
-                                        Id = cf.CollectionPointCustomFieldId.Value,
-                                        IsRequired = cf.Required.Value,
-                                        Sequence = cf.Sequence.Value,
-                                    }).ToList();
-
-
+        var customFieldInfo = (from cf in _context.DbSetConsentCollectionPointCustomFields
+                               join cpf in _context.DbSetConsent_CollectionPointCustomFieldConfig on cf.CollectionPointCustomFieldId equals cpf.CollectionPointCustomFieldId
+                               where collectionpointInfo.Guid.ToString() == cpf.CollectionPointGuid && cf.CompanyId == 1 && cf.Status != Status.X.ToString()
+                               select new CustomFields
+                               {
+                                   Id = cf.CollectionPointCustomFieldId,
+                                   IsRequired = cpf.Required.Value,
+                                   Sequence = cpf.Sequence.Value,
+                                   InputType = cf.Type,
+                                   Title = cf.Description,
+                                   Placeholder = cf.Placeholder,
+                                   LengthLimit = cf.LengthLimit,
+                                   MaxLines = cf.MaxLines,
+                                   MinLines = cf.MinLines,
+                               }).ToList();
         #endregion
 
         #region page details
         var pageDetails = (from cp in _context.DbSetConsentPage
                            join p in _context.DbSetConsentCollectionPoints on cp.CollectionPointId equals p.CollectionPointId
-                           where cp.Status == Status.Active.ToString() && cp.CollectionPointId == collectionpointInfo.CollectionPointId
+                           where cp.Status == Status.Active.ToString() && cp.CollectionPointId == request.Id && cp.CompanyId == 1 && cp.Status != Status.X.ToString()
 
                            select
                            new CollectionPointPageDetail
-                           {
-                               /*AcceptCheckBoxLabel = cp.LabelCheckBoxAccept,
-                               AcceptCheckBoxLabelFontColor = cp.LabelCheckBoxAcceptFontColor,
-                               BodyBackgroundColor = cp.BodyBgcolor,*/
-                               // BodyBackgroundId 
-                               /*BodyBackground = cp.BodyBgcolor,
-                               BodyBottomDescription = cp.BodyBottomDescription,
-                               BodyBottomDescriptionFontColor = cp.BodyBottomDescriptionFontColor,
-                               BodyTopDescription = cp.BodyTopDescription,
-                               BodyTopDerscriptionFontColor = cp.BodyTopDerscriptionFontColor,
-                               CancelButtonBackgroundColor = cp.CancelButtonBgcolor,
-                               CancelButtonFontColor = cp.CancelButtonFontColor,
-                               CancelButtonLabel = cp.LabelActionCancel,*/
-                               //ConfirmButtonLabel 
-                               //HeaderBackgroundColor = cp.HeaderBgcolor,
-                               //HeaderBackgroundId 
-                               // HeaderBackground
-                               /*HeaderFontColor = cp.HeaderFontColor,
-                               HeaderLabel = cp.HeaderLabel,*/
-                               //Logo,
-                               //LogoId,
-                               /*OkButtonBackgroundColor = cp.OkbuttonBgcolor,
-                               OkButtonFontColor = cp.OkbuttonFontColor,*/
+                           {   
+                               
+                               AcceptCheckBoxText = cp.LabelCheckBoxAccept,
+                               BackgroundImageId = cp.BodyBgimage,
+                               BodyBottomDescriptionText = cp.BodyBottomDescription,
+                               BodyTopDescriptionText = cp.BodyTopDescription,
+                               CancelButtonText = cp.CancelButtonBgcolor,
+                               ConfirmButtonText = cp.OkbuttonBgcolor,
+                               HeaderBackgroundImageId = cp.HeaderBgimage,
+                               HeaderText = cp.HeaderLabel,
+                               LogoImageId = cp.HeaderLogo,
+                               PolicyUrlText = cp.LabelLinkToPolicy,
                                PolicyUrl = cp.LabelLinkToPolicyUrl,
-                               /*PolicyUrlLabel = cp.LabelLinkToPolicyUrl,
-                               PurposeAcceptLabel = cp.LabelPurposeActionAgree,
-                               PolicyUrlLabelFontColor = cp.LabelLinkToPolicyFontColor,
-                               PurposeRejectLabel = cp.LabelPurposeActionNotAgree,*/
                                RedirectUrl = cp.RedirectUrl,
-                               /*SuccessHeaderLabel = cp.HeaderLabelThankPage,
-                               SuccessDescription = cp.ShortDescriptionThankPage,
-                               SuccessButtonLabel = cp.ButtonThankpage*/
+                               SuccessHeaderText = cp.HeaderLabelThankPage,
+                               SuccessDescriptionText = cp.BodyBottomDescription,
+                               SuccessButtonText = cp.ButtonThankpage,
+
                            }).FirstOrDefault();
 
         #endregion
 
+        collectionpointInfo.CollectionPointInfoWebsiteObject = websiteList;
         collectionpointInfo.PurposeList = purposeList;
-        //collectionpointInfo.CustomFieldsList = customFieldsList;
+        collectionpointInfo.CustomFieldsList = customFieldInfo;
         collectionpointInfo.PageDetail = pageDetails;
 
         return collectionpointInfo;
