d7d9bdb11c4f84acc3a6efe7fdcdd15eb5059c43
Gun Jiraphat <sriyuyongj@gmail.com>
2023-06-27 13:39:52 +0700

    add : Athorization fix : Command

--- src/Application/Permission/Queries/GetFrequentlyUsedMenu/GetFrequentluUsedMenuQuery.cs
+++ src/Application/Permission/Queries/GetFrequentlyUsedMenu/GetFrequentluUsedMenuQuery.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 using AutoMapper;
 using FluentValidation.Results;
@@ -9,11 +10,16 @@ using MediatR;
 using Wisework.ConsentManagementSystem.Api;
 using WW.Application.Common.Exceptions;
 using WW.Application.Common.Interfaces;
+using WW.Application.Common.Models;
 
 namespace WW.Application.Permission.Queries.GetFrequentlyUsedMenu;
 
 
-public record GetFrequentluUsedMenuQuery(int count) : IRequest<List<FrequentlyUsedMenu>>;
+public record GetFrequentluUsedMenuQuery(int count) : IRequest<List<FrequentlyUsedMenu>>
+{
+    [JsonIgnore]
+    public AuthenticationModel? authentication { get; set; }
+}
 
 public class GetFrequentluUsedMenuQueryHandler : IRequestHandler<GetFrequentluUsedMenuQuery, List<FrequentlyUsedMenu>>
 {
@@ -28,6 +34,10 @@ public class GetFrequentluUsedMenuQueryHandler : IRequestHandler<GetFrequentluUs
 
     public async Task<List<FrequentlyUsedMenu>> Handle(GetFrequentluUsedMenuQuery request, CancellationToken cancellationToken)
     {
+        if (request.authentication == null)
+        {
+            throw new UnauthorizedAccessException();
+        }
         var actions = _context.DbSetAction;
         var programs = _context.DbSetProgram;
         var programActions = _context.DbSetProgramAction;
@@ -39,7 +49,13 @@ public class GetFrequentluUsedMenuQueryHandler : IRequestHandler<GetFrequentluUs
         string baseUrl = "https://test-pdpa.thewiseworks.com/";
         string language = "en-US";
         List<int> roleIDs = new List<int> { 4, 50168, 50169 };
-        int companyId = 1;
+        int companyId = request.authentication.CompanyID;
+        int userId = request.authentication.UserID;
+
+        var roleIds = userRoles
+            .Where(ur => ur.UserID == userId)
+            .Select(ur => ur.RoleID)
+            .ToList();
 
         try
         {
@@ -62,7 +78,7 @@ public class GetFrequentluUsedMenuQueryHandler : IRequestHandler<GetFrequentluUs
                              join pa in programActions on rpa.ProgramActionID equals pa.ProgramActionID
                              join p2 in programs on pa.ProgramID equals p2.ProgramID
                              join a in actions on pa.ActionID equals a.ActionID
-                             where roleIDs.Contains(Convert.ToInt32(rpa.RoleID)) && r.CompanyID == companyId && p2.Code == p1.Code && a.Code == "VIEW" && r.Status != "X"
+                             where roleIds.Contains(Convert.ToInt32(rpa.RoleID)) && r.CompanyID == companyId && p2.Code == p1.Code && a.Code == "VIEW" && r.Status != "X"
                              select 1
                              ).Count() > 0
                          ) || p1.Code == "HOME_MAIN"
@@ -78,55 +94,23 @@ public class GetFrequentluUsedMenuQueryHandler : IRequestHandler<GetFrequentluUs
                              ProgramID = p1.ProgramID,
                              IsExpanded = p1.expanded == null || p1.expanded == 0 ? false : true,
                              Icon = p1.Icon,
+                             
                          }
                     ).Take(request.count).ToList();
 
-            //if (menus == null) return new List<FrequentlyUsedMenu>();
+            if (menus == null) return new List<FrequentlyUsedMenu>();
 
-            
+
             if (menus == null)
             {
                 throw new NotFoundException("User not found"); // 404 Not Found
             }
 
-            /*
-
-            SELECT * FROM [Action];
-            SELECT * FROM [Program];
-            SELECT * FROM [ProgramAction];
-            SELECT * FROM [ProgramDescription];
-            SELECT * FROM [Role];
-            SELECT * FROM [RoleProgramAction];
-
-            SELECT 
-                P1.ProgramId, P1.Code, P1.Description, P1.ParentID, 
-                P1.Action, P1.Icon, P1.Badge, P1.expanded, P1.Priority
-            FROM [Program] P1
-            LEFT JOIN (
-                SELECT * FROM [ProgramDescription] WHERE LanguageCulture = 'en-US'
-            ) PD ON PD.ProgramID = P1.ProgramID
-            WHERE P1.Status = 'A'
-            AND (
-                (
-                    SELECT (CASE WHEN COUNT(*) > 0 THEN 1 ELSE 0 END)
-                    FROM [RoleProgramAction] RPA
-                    LEFT JOIN [Role] R ON R.RoleID = RPA.RoleID
-                    LEFT JOIN [ProgramAction] PA ON PA.ProgramActionID = RPA.ProgramActionID
-                    LEFT JOIN [Program] P2 ON P2.ProgramID = PA.ProgramID
-                    LEFT JOIN [Action] A ON A.ActionID = PA.ActionID
-                    WHERE RPA.RoleID IN (4, 50168, 50169)
-                    AND R.CompanyID = 1
-                    AND P2.Code = P1.Code
-                    AND A.Code = 'VIEW'
-                    AND R.Status != 'X'
-                ) = 1
-                OR (P1.Code = 'HOME_MAIN')
-            )
-            ORDER BY P1.Priority;
-
-            */
-
             return menus;
+        }
+        catch (UnauthorizedAccessException)
+        {
+            throw new UnauthorizedAccessException();
         }
         catch (NotFoundException)
         {
--- src/Application/Permission/Queries/GetMenu/GetMenuQuery.cs
+++ src/Application/Permission/Queries/GetMenu/GetMenuQuery.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 using AutoMapper;
 using FluentValidation.Results;
@@ -10,10 +11,15 @@ using Microsoft.EntityFrameworkCore;
 using Wisework.ConsentManagementSystem.Api;
 using WW.Application.Common.Exceptions;
 using WW.Application.Common.Interfaces;
+using WW.Application.Common.Models;
 using static System.Net.WebRequestMethods;
 
 namespace WW.Application.Permission.Queries.GetMenu;
-public record GetMenuQuery : IRequest<List<Menu>>;
+public record GetMenuQuery : IRequest<List<Menu>>
+{
+    [JsonIgnore]
+    public AuthenticationModel? authentication { get; set; }
+}
 
 public class GetMenuQueryHandler : IRequestHandler<GetMenuQuery, List<Menu>>
 {
@@ -28,6 +34,11 @@ public class GetMenuQueryHandler : IRequestHandler<GetMenuQuery, List<Menu>>
 
     public async Task<List<Menu>> Handle(GetMenuQuery request, CancellationToken cancellationToken)
     {
+        if (request.authentication == null)
+        {
+            throw new UnauthorizedAccessException();
+        }
+
         var actions = _context.DbSetAction;
         var programs = _context.DbSetProgram;
         var programActions = _context.DbSetProgramAction;
@@ -39,8 +50,8 @@ public class GetMenuQueryHandler : IRequestHandler<GetMenuQuery, List<Menu>>
 
         string baseUrl = "https://test-pdpa.thewiseworks.com/";
         string language = "en-US";
-        int companyId = 1;
-        int userId = 1;
+        int companyId = request.authentication.CompanyID;
+        int userId = request.authentication.UserID;
 
         var roleIds = userRoles
             .Where(ur => ur.UserID == userId)
@@ -51,7 +62,7 @@ public class GetMenuQueryHandler : IRequestHandler<GetMenuQuery, List<Menu>>
 
         try
         {
-            if (roleIDs == null)
+            if (roleIds == null)
             {
                 List<ValidationFailure> failures = new List<ValidationFailure> { };
 
@@ -61,48 +72,48 @@ public class GetMenuQueryHandler : IRequestHandler<GetMenuQuery, List<Menu>>
             }
 
             var menus = (from p1 in programs
-                     join pd in programDescriptions.Where(d => d.LanguageCulture == language)
-                     on p1.ProgramID equals pd.ProgramID into pdGroup
-                     from pd in pdGroup.DefaultIfEmpty()
-                     where p1.Status == "A" && (
-                        (from rpa in roleProgramActions
-                         join r in roles on rpa.RoleID equals r.RoleID
-                         join pa in programActions on rpa.ProgramActionID equals pa.ProgramActionID
-                         join p2 in programs on pa.ProgramID equals p2.ProgramID
-                         join a in actions on pa.ActionID equals a.ActionID
-                         where roleIDs.Contains(Convert.ToInt32(rpa.RoleID)) && r.CompanyID == companyId && p2.Code == p1.Code && a.Code == "VIEW" && r.Status != "X"
-                         select 1
-                         ).Count() > 0
-                     ) || p1.Code == "HOME_MAIN"
-                     orderby p1.Priority
-                     let parentId = p1.ProgramID // Store p1.ProgramID in a separate variable
-                     select new Menu
-                     {
-                         Action = baseUrl + p1.Action,
-                         Code = p1.Code,
-                         Description = p1.Description,
-                         ParentID = p1.ParentID,
-                         Priority = p1.Priority,
-                         ProgramGroupID = 20, // what?
-                         ProgramID = p1.ProgramID,
-                         IsExpanded = p1.expanded == null || p1.expanded == 0 ? false : true,
-                         Icon = p1.Icon,
-                         Items = (from childProgram in programs
-                                     //where childProgram.ParentID == p1.ProgramID
-                                     select new SubMenu
-                                     {
-                                         Action = baseUrl + childProgram.Action,
-                                         Code = childProgram.Code,
-                                         Description = childProgram.Description,
-                                         ParentID = childProgram.ParentID,
-                                         Priority = childProgram.Priority,
-                                         ProgramGroupID = 20, // what?
-                                         ProgramID = childProgram.ProgramID,
-                                         IsExpanded = childProgram.expanded == null || childProgram.expanded == 0 ? false : true,
-                                         Icon = childProgram.Icon
-                                     }).ToList().Find(x => x.ProgramID == p1.ProgramID)
+                         join pd in programDescriptions.Where(d => d.LanguageCulture == language)
+                         on p1.ProgramID equals pd.ProgramID into pdGroup
+                         from pd in pdGroup.DefaultIfEmpty()
+                         where p1.Status == "A" && (
+                            (from rpa in roleProgramActions
+                             join r in roles on rpa.RoleID equals r.RoleID
+                             join pa in programActions on rpa.ProgramActionID equals pa.ProgramActionID
+                             join p2 in programs on pa.ProgramID equals p2.ProgramID
+                             join a in actions on pa.ActionID equals a.ActionID
+                             where roleIds.Contains(Convert.ToInt32(rpa.RoleID)) && r.CompanyID == companyId && p2.Code == p1.Code && a.Code == "VIEW" && r.Status != "X"
+                             select 1
+                             ).Count() > 0
+                         ) || p1.Code == "HOME_MAIN"
+                         orderby p1.Priority
+                         let parentId = p1.ProgramID // Store p1.ProgramID in a separate variable
+                         select new Menu
+                         {
+                             Action = baseUrl + p1.Action,
+                             Code = p1.Code,
+                             Description = p1.Description,
+                             ParentID = p1.ParentID,
+                             Priority = p1.Priority,
+                             ProgramGroupID = 20, // what?
+                             ProgramID = p1.ProgramID,
+                             IsExpanded = p1.expanded == null || p1.expanded == 0 ? false : true,
+                             Icon = p1.Icon,
+                             Items = (from childProgram in programs
+                                      where childProgram.ParentID == p1.ProgramID
+                                      select new SubMenu
+                                      {
+                                          Action = baseUrl + childProgram.Action,
+                                          Code = childProgram.Code,
+                                          Description = childProgram.Description,
+                                          ParentID = childProgram.ParentID,
+                                          Priority = childProgram.Priority,
+                                          ProgramGroupID = 20, // what?
+                                          ProgramID = childProgram.ProgramID,
+                                          IsExpanded = childProgram.expanded == null || childProgram.expanded == 0 ? false : true,
+                                          Icon = childProgram.Icon
+                                      }).ToList()
 
-                     }).ToList();
+                         }).ToList();
 
          List<SubMenu> GetChildren( int parentID)
         {
@@ -125,46 +136,11 @@ public class GetMenuQueryHandler : IRequestHandler<GetMenuQuery, List<Menu>>
 
         if (menus == null) return new List<Menu>();
 
-        /*
-
-        SELECT * FROM [Action];
-        SELECT * FROM [ActionLog];
-        SELECT * FROM [Program];
-        SELECT * FROM [ProgramAction];
-        SELECT * FROM [ProgramDescription];
-        SELECT * FROM [Role];
-        SELECT * FROM [RoleProgramAction];
-        SELECT * FROM [UserRole];
-        
-        SELECT 
-	        P1.ProgramId, P1.Code, P1.Description, P1.ParentID, 
-	        P1.Action, P1.Icon, P1.Badge, P1.expanded, P1.Priority
-        FROM [Program] P1
-        LEFT JOIN (
-	        SELECT * FROM [ProgramDescription] WHERE LanguageCulture = 'en-US'
-        ) PD ON PD.ProgramID = P1.ProgramID
-        WHERE P1.Status = 'A'
-        AND (
-	        (
-		        SELECT (CASE WHEN COUNT(*) > 0 THEN 1 ELSE 0 END)
-		        FROM [RoleProgramAction] RPA
-		        LEFT JOIN [Role] R ON R.RoleID = RPA.RoleID
-		        LEFT JOIN [ProgramAction] PA ON PA.ProgramActionID = RPA.ProgramActionID
-		        LEFT JOIN [Program] P2 ON P2.ProgramID = PA.ProgramID
-		        LEFT JOIN [Action] A ON A.ActionID = PA.ActionID
-		        WHERE RPA.RoleID IN (4, 50168, 50169)
-		        AND R.CompanyID = 1
-		        AND P2.Code = P1.Code
-		        AND A.Code = 'VIEW'
-		        AND R.Status != 'X'
-	        ) = 1
-	        OR (P1.Code = 'HOME_MAIN')
-        )
-        ORDER BY P1.Priority;
-
-        */
-
         return menus;
+        }
+        catch (UnauthorizedAccessException)
+        {
+            throw new UnauthorizedAccessException();
         }
         catch (NotFoundException)
         {
@@ -183,5 +159,6 @@ public class GetMenuQueryHandler : IRequestHandler<GetMenuQuery, List<Menu>>
         {
             throw new InternalException("An internal server error occurred."); // 500 error
         }
+
     }
 }
