aabad5f31ac88a35731b339d8aeb43e0f753e386
Panyawut Suton <92622255+BossPanyawut@users.noreply.github.com>
2023-07-04 13:39:29 +0700

    Added error handle for website API.

--- src/Application/Website/Commands/UpdateWebsite/UpdateWebsiteCommands.cs
+++ src/Application/Website/Commands/UpdateWebsite/UpdateWebsiteCommands.cs
@@ -2,10 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
+using FluentValidation.Results;
 using MediatR;
 using Wisework.ConsentManagementSystem.Api;
+using WW.Application.Common.Exceptions;
 using WW.Application.Common.Interfaces;
+using WW.Application.Common.Models;
 using WW.Application.CustomField.Commands.UpdateCustomField;
 using WW.Domain.Entities;
 using WW.Domain.Enums;
@@ -15,11 +19,15 @@ namespace WW.Application.Website.Commands.UpdateWebsite;
 
 public record UpdateWebsiteCommands : IRequest<WebsiteActiveList>
 {
-    public int WebsiteId { get; init; }
+    [JsonIgnore]
+    public int WebsiteId { get; set; }
     public string Name { get; init; }
     public string UrlHomePage { get; init; }
     public string UrlPolicyPage { get; init; }
     public string Status { get; init; }
+
+    [JsonIgnore]
+    public AuthenticationModel? authentication { get; set; }
 }
 
 public class UpdateWebsiteCommandsHandler : IRequestHandler<UpdateWebsiteCommands, WebsiteActiveList>
@@ -33,39 +41,62 @@ public class UpdateWebsiteCommandsHandler : IRequestHandler<UpdateWebsiteCommand
 
     public async Task<WebsiteActiveList> Handle(UpdateWebsiteCommands request, CancellationToken cancellationToken)
     {
+
+        if (request.authentication == null)
+        {
+            throw new UnauthorizedAccessException();
+        }
+
+        if (request.WebsiteId <= 0)
+        {
+            List<ValidationFailure> failures = new List<ValidationFailure> { };
+            failures.Add(new ValidationFailure("PurposeID", "Purpose ID must be greater than 0"));
+
+            throw new ValidationException(failures);
+        }
+
         var entity = _context.DbSetConsentWebsite
              .Where(cf => cf.WebsiteId == request.WebsiteId && cf.CompanyId == 1 && cf.Status != Status.X.ToString())
              .FirstOrDefault();
 
         if (entity == null)
         {
-            return new WebsiteActiveList();
+            throw new NotFoundException();
         }
 
-        entity.Description = request.Name;
-        entity.Url = request.UrlHomePage;
-        entity.Urlpolicy = request.UrlPolicyPage;
-        entity.Status = Status.Active.ToString();
-
-        entity.UpdateBy = 1;
-        entity.UpdateDate = DateTime.Now;
-
-        
-        entity.Version = 1;
-        
-
-        _context.DbSetConsentWebsite.Update(entity);
-
-        await _context.SaveChangesAsync(cancellationToken);
-
-        var websiteInfo = new WebsiteActiveList
+        try
         {
-            WebsiteId = entity.WebsiteId,
-            Name = entity.Description,
-            UrlHomePage = entity.Url,
-            UrlPolicyPage = entity.Urlpolicy,
-        };
+            entity.Description = request.Name;
+            entity.Url = request.UrlHomePage;
+            entity.Urlpolicy = request.UrlPolicyPage;
+            entity.Status = Status.Active.ToString();
 
-        return websiteInfo;
+            entity.UpdateBy = 1;
+            entity.UpdateDate = DateTime.Now;
+
+
+            entity.Version = 1;
+
+
+            _context.DbSetConsentWebsite.Update(entity);
+
+            await _context.SaveChangesAsync(cancellationToken);
+
+            var websiteInfo = new WebsiteActiveList
+            {
+                WebsiteId = entity.WebsiteId,
+                Name = entity.Description,
+                UrlHomePage = entity.Url,
+                UrlPolicyPage = entity.Urlpolicy,
+            };
+
+            return websiteInfo;
+        }
+        catch (Exception ex)
+        {
+            throw new InternalServerException(ex.Message);
+        }
+
+       
     }
 }
--- src/Application/Website/Queries/GetWebsite/GetWebsiteQuery.cs
+++ src/Application/Website/Queries/GetWebsite/GetWebsiteQuery.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 using AutoMapper;
 using AutoMapper.QueryableExtensions;
+using FluentValidation.Results;
 using MediatR;
 using Wisework.ConsentManagementSystem.Api;
+using WW.Application.Common.Exceptions;
 using WW.Application.Common.Interfaces;
 using WW.Application.Common.Mappings;
 using WW.Application.Common.Models;
@@ -16,8 +19,11 @@ using WW.Domain.Enums;
 namespace WW.Application.Website.Queries.GetWebsite;
 public record GetWebsiteQuery: IRequest<PaginatedList<WebsiteActiveList>>
 {
-    public int Offset { get; init; } = 1;
-    public int Limit { get; init; } = 10;
+    public int offset { get; init; } = 1;
+    public int limit { get; init; } = 10;
+
+    [JsonIgnore]
+    public AuthenticationModel? authentication { get; set; }
 }
 public class GetWebsiteQueryHandler : IRequestHandler<GetWebsiteQuery, PaginatedList<WebsiteActiveList>>
 {
@@ -30,21 +36,51 @@ public class GetWebsiteQueryHandler : IRequestHandler<GetWebsiteQuery, Paginated
     }
     public async Task<PaginatedList<WebsiteActiveList>> Handle(GetWebsiteQuery request, CancellationToken cancellationToken)
     {
-        MapperConfiguration config = new MapperConfiguration(cfg =>
+        if (request.authentication == null)
         {
-            cfg.CreateMap<ConsentWebsite, WebsiteActiveList>().ForMember(d=>d.WebsiteId, a=>a.MapFrom(s=>s.WebsiteId))
-            .ForMember(d => d.Name, a => a.MapFrom(s => s.Description))
-            .ForMember(d => d.UrlHomePage, a => a.MapFrom(s => s.Url))
-            .ForMember(d => d.UrlPolicyPage, a => a.MapFrom(s => s.Urlpolicy))
-            ;
-        });
+            throw new UnauthorizedAccessException();
+        }
 
-        Mapper mapper = new Mapper(config);
+        if (request.offset <= 0 || request.limit <= 0)
+        {
+            List<ValidationFailure> failures = new List<ValidationFailure> { };
 
-        //todo:edit conpanyid หลังมีการทำ identity server
-        PaginatedList<WebsiteActiveList> model =
-            await _context.DbSetConsentWebsite.Where(w => w.CompanyId == 1 && w.Status == Status.Active.ToString()).ProjectTo<WebsiteActiveList>(mapper.ConfigurationProvider).PaginatedListAsync(request.Offset, request.Limit);
+            if (request.offset <= 0)
+            {
+                failures.Add(new ValidationFailure("offset", "Offset must be greater than 0"));
+            }
+            if (request.limit <= 0)
+            {
+                failures.Add(new ValidationFailure("limit", "Limit must be greater than 0"));
+            }
 
-        return model;
+            throw new ValidationException(failures);
+        }
+
+        try
+        {
+            MapperConfiguration config = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<ConsentWebsite, WebsiteActiveList>().ForMember(d => d.WebsiteId, a => a.MapFrom(s => s.WebsiteId))
+                .ForMember(d => d.Name, a => a.MapFrom(s => s.Description))
+                .ForMember(d => d.UrlHomePage, a => a.MapFrom(s => s.Url))
+                .ForMember(d => d.UrlPolicyPage, a => a.MapFrom(s => s.Urlpolicy))
+                ;
+            });
+
+            Mapper mapper = new Mapper(config);
+
+            //todo:edit conpanyid หลังมีการทำ identity server
+            PaginatedList<WebsiteActiveList> model =
+                await _context.DbSetConsentWebsite.Where(w => w.CompanyId == 1 && w.Status == Status.Active.ToString()).ProjectTo<WebsiteActiveList>(mapper.ConfigurationProvider).PaginatedListAsync(request.offset, request.limit);
+
+            return model;
+        }
+        catch (Exception ex)
+        {
+            throw new InternalServerException(ex.Message);
+        }
+
+       
     }
 }
