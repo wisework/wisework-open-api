7e7665fb0ee31732a54c31ec2bf5a69aa8697e6f
Panyawut Suton <92622255+BossPanyawut@users.noreply.github.com>
2023-07-07 14:48:29 +0700

    apply handling of section errors

--- src/Application/Section/Queries/GetSection/GetSectionInfoQuery.cs
+++ src/Application/Section/Queries/GetSection/GetSectionInfoQuery.cs
@@ -2,15 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 using AutoMapper;
+using FluentValidation.Results;
 using MediatR;
 using Wisework.ConsentManagementSystem.Api;
+using WW.Application.Common.Exceptions;
 using WW.Application.Common.Interfaces;
+using WW.Application.Common.Models;
 using WW.Domain.Entities;
 
 namespace WW.Application.Section.Queries.GetSection;
-public record GetSectionInfoQuery(int Id) : IRequest<SectionActiveList>;
+public record GetSectionInfoQuery(int SectionId) : IRequest<SectionActiveList>
+{
+    [JsonIgnore]
+    public AuthenticationModel? authentication { get; set; }
+};
 
 public class GetSectionInfoQueryHandler : IRequestHandler<GetSectionInfoQuery, SectionActiveList>
 {
@@ -25,6 +33,18 @@ public class GetSectionInfoQueryHandler : IRequestHandler<GetSectionInfoQuery, S
 
     public async Task<SectionActiveList> Handle(GetSectionInfoQuery request, CancellationToken cancellationToken)
     {
+        if (request.authentication == null)
+        {
+            throw new UnauthorizedAccessException();
+        }
+
+        if (request.SectionId <= 0)
+        {
+            List<ValidationFailure> failures = new List<ValidationFailure> { };
+            failures.Add(new ValidationFailure("sectionID", "Section ID must be greater than 0"));
+
+            throw new ValidationException(failures);
+        }
 
         MapperConfiguration config = new MapperConfiguration(cfg =>
         {
@@ -35,21 +55,35 @@ public class GetSectionInfoQueryHandler : IRequestHandler<GetSectionInfoQuery, S
 
         //todo:edit conpanyid หลังมีการทำ identity server
         var sectionInfo = (from ppc in _context.DbSetConsentSectionInfo
-                                   join uCreate in _context.DbSetUser on ppc.CreateBy equals uCreate.CreateBy
-                                   join uUpdate in _context.DbSetUser on ppc.CreateBy equals uUpdate.UpdateBy
-                                   join c in _context.DbSetCompanies on ppc.CompanyId equals (int)(long)c.CompanyId
-                                   join w in _context.DbSetConsentWebsite on (int)(long)c.CompanyId equals w.CompanyId
-                                   where ppc.SectionInfoId == request.Id
-                                   select new SectionActiveList
-                                   {
-                                       SectionId = request.Id,
-                                       Code = ppc.Code,
-                                       Description = ppc.Description,
-                                       Status = ppc.Status
+                           join uCreate in _context.DbSetUser on ppc.CreateBy equals uCreate.CreateBy
+                           join uUpdate in _context.DbSetUser on ppc.CreateBy equals uUpdate.UpdateBy
+                           join c in _context.DbSetCompanies on ppc.CompanyId equals (int)(long)c.CompanyId
+                           join w in _context.DbSetConsentWebsite on (int)(long)c.CompanyId equals w.CompanyId
+                           where ppc.SectionInfoId == request.SectionId
+                           select new SectionActiveList
+                           {
+                               SectionId = request.SectionId,
+                               Code = ppc.Code,
+                               Description = ppc.Description,
+                               Status = ppc.Status
 
-                                   }).FirstOrDefault();
+                           }).FirstOrDefault();
 
-        return sectionInfo;
+        if (sectionInfo == null)
+        {
+            throw new NotFoundException();
+        }
+
+        try
+        {
+            return sectionInfo;
+        }
+        catch (Exception ex)
+        {
+            throw new InternalServerException(ex.Message);
+        }
+
+      
     }
 
 }
--- src/Application/Section/Queries/GetSection/GetSectionQuery.cs
+++ src/Application/Section/Queries/GetSection/GetSectionQuery.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 using AutoMapper;
 using AutoMapper.QueryableExtensions;
+using FluentValidation.Results;
 using MediatR;
 using Wisework.ConsentManagementSystem.Api;
+using WW.Application.Common.Exceptions;
 using WW.Application.Common.Interfaces;
 using WW.Application.Common.Mappings;
 using WW.Application.Common.Models;
@@ -19,6 +22,9 @@ public record GetSectionQuery : IRequest<PaginatedList<SectionActiveList>>
 {
     public int offset { get; init; } = 1;
     public int limit { get; init; } = 10;
+
+    [JsonIgnore]
+    public AuthenticationModel? authentication { get; set; }
 }
 public class GetSectionQueryHandler : IRequestHandler<GetSectionQuery, PaginatedList<SectionActiveList>>
 {
@@ -31,18 +37,49 @@ public class GetSectionQueryHandler : IRequestHandler<GetSectionQuery, Paginated
     }
     public async Task<PaginatedList<SectionActiveList>> Handle(GetSectionQuery request, CancellationToken cancellationToken)
     {
-        MapperConfiguration config = new MapperConfiguration(cfg =>
+
+        if (request.authentication == null)
         {
-            cfg.CreateMap<Consent_SectionInfo, SectionActiveList>().ForMember(d => d.SectionId, a => a.MapFrom(s => s.SectionInfoId));
-        });
+            throw new UnauthorizedAccessException();
+        }
 
-        Mapper mapper = new Mapper(config);
+        if (request.offset <= 0 || request.limit <= 0)
+        {
+            List<ValidationFailure> failures = new List<ValidationFailure> { };
 
-        //todo:edit conpanyid หลังมีการทำ identity server
-        PaginatedList<SectionActiveList> model =
-            await _context.DbSetConsentSectionInfo.Where(p => p.CompanyId == 1 && p.Status == Status.Active.ToString())
-            .ProjectTo<SectionActiveList>(mapper.ConfigurationProvider).PaginatedListAsync(request.offset, request.limit);
+            if (request.offset <= 0)
+            {
+                failures.Add(new ValidationFailure("offset", "Offset must be greater than 0"));
+            }
+            if (request.limit <= 0)
+            {
+                failures.Add(new ValidationFailure("limit", "Limit must be greater than 0"));
+            }
 
-        return model;
+            throw new ValidationException(failures);
+        }
+
+        try
+        {
+            MapperConfiguration config = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<Consent_SectionInfo, SectionActiveList>().ForMember(d => d.SectionId, a => a.MapFrom(s => s.SectionInfoId));
+            });
+
+            Mapper mapper = new Mapper(config);
+
+            //todo:edit conpanyid หลังมีการทำ identity server
+            PaginatedList<SectionActiveList> model =
+                await _context.DbSetConsentSectionInfo.Where(p => p.CompanyId == request.authentication.CompanyID && p.Status == Status.Active.ToString())
+                .ProjectTo<SectionActiveList>(mapper.ConfigurationProvider).PaginatedListAsync(request.offset, request.limit);
+
+            return model;
+        }
+        catch  (Exception ex)
+        {
+            throw new InternalServerException(ex.Message);
+        }
+
+     
     }
 }
