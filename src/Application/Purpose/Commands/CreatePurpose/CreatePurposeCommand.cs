b762c534a2ae61d4c96c2249cba42d721afd70a2
Panyawut Suton <92622255+BossPanyawut@users.noreply.github.com>
2023-07-13 06:55:20 +0700

    feature/add purpose API

---
+++ src/Application/Purpose/Commands/CreatePurpose/CreatePurposeCommand.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using MediatR;
+using Wisework.ConsentManagementSystem.Api;
+using WW.Application.Common.Exceptions;
+using WW.Application.Common.Interfaces;
+using WW.Application.Common.Models;
+using WW.Application.CustomField.Commands.CreateCustomField;
+using WW.Domain.Common;
+using WW.Domain.Entities;
+using WW.Domain.Enums;
+
+namespace WW.Application.Purpose.Commands.CreatePurpose;
+
+public record CreatePurposeCommand : IRequest<PurposeActiveList>
+{
+    public int purposeType { get; init; }
+    public int purposeCategoryId { get; init; }
+    public string code { get; init; }
+    public string description { get; init; }
+    public string keepAliveData { get; init; }
+    public string linkMoreDetail { get; init; }
+    public string status { get; init; }
+    public string textMoreDetail { get; init; }
+    public string warningDescription { get; init; }
+    [JsonIgnore]
+    public AuthenticationModel? authentication { get; set; }
+}
+
+public class CreatePurposeCommandHandler : IRequestHandler<CreatePurposeCommand, PurposeActiveList>
+{
+    private readonly IApplicationDbContext _context;
+
+    public CreatePurposeCommandHandler(IApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<PurposeActiveList> Handle(CreatePurposeCommand request, CancellationToken cancellationToken)
+    {
+        if (request.authentication == null)
+        {
+            throw new UnauthorizedAccessException();
+        }
+
+        try
+        {
+            var entity = new Consent_Purpose();
+
+            entity.Guid = Guid.NewGuid().ToString();
+            entity.PurposeType = request.purposeType;
+            entity.PurposeCategoryId = request.purposeCategoryId;
+            entity.Code = request.code;
+            entity.Description = request.description;
+            entity.KeepAliveData = request.keepAliveData;
+            entity.LinkMoreDetail = request.linkMoreDetail;
+            entity.TextMoreDetail = request.textMoreDetail;
+            entity.WarningDescription = request.warningDescription;
+
+            entity.CreateBy = request.authentication.UserID;
+            entity.UpdateBy = request.authentication.UserID;
+            entity.CreateDate = DateTime.Now;
+            entity.UpdateDate = DateTime.Now;
+
+            entity.Status = Status.Active.ToString();
+            entity.Version = 1;
+            entity.CompanyId = request.authentication.CompanyID;
+            entity.Language = "en";
+            entity.ExpiredDateTime = $"{Calulate.ExpiredDateTime(request.keepAliveData, DateTime.Now)}";
+
+            _context.DbSetConsentPurpose.Add(entity);
+
+            await _context.SaveChangesAsync(cancellationToken);
+
+            var PurposeInfo = new PurposeActiveList
+            {
+                GUID = new Guid(entity.Guid),
+                PurposeID = entity.PurposeId,
+                PurposeType = entity.PurposeType,
+                CategoryID = entity.PurposeCategoryId,
+                Code = entity.Code,
+                Description = entity.Description,
+                KeepAliveData = entity.KeepAliveData,
+                LinkMoreDetail = entity.LinkMoreDetail,
+                Status = entity.Status,
+                TextMoreDetail = entity.TextMoreDetail,
+                WarningDescription = entity.WarningDescription,
+                Language = entity.Language,
+                ExpiredDateTime = entity.ExpiredDateTime,
+            };
+
+            return PurposeInfo;
+        }
+        catch (Exception ex)
+        {
+            throw new InternalServerException(ex.Message);
+        }
+    }
+}
---
+++ src/Application/Purpose/Queries/GetPurpose/GetPurposeQuery.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using FluentValidation.Results;
+using MediatR;
+using Wisework.ConsentManagementSystem.Api;
+using WW.Application.Common.Exceptions;
+using WW.Application.Common.Interfaces;
+using WW.Application.Common.Mappings;
+using WW.Application.Common.Models;
+using WW.Application.CustomField.Queries.GetCustomField;
+using WW.Domain.Common;
+using WW.Domain.Entities;
+using WW.Domain.Enums;
+
+namespace WW.Application.Purpose.Queries.GetPurpose;
+
+public record GetPurposeQuery : IRequest<PaginatedList<PurposeActiveList>>
+{
+    public int offset { get; init; } = 1;
+    public int limit { get; init; } = 10;
+    [JsonIgnore]
+    public AuthenticationModel? authentication { get; set; }
+}
+
+public class GetPurposeQueryHandler : IRequestHandler<GetPurposeQuery, PaginatedList<PurposeActiveList>>
+{
+    private readonly IApplicationDbContext _context;
+    private readonly IMapper _mapper;
+    public GetPurposeQueryHandler(IApplicationDbContext context, IMapper mapper)
+    {
+        _context = context;
+        _mapper = mapper;
+    }
+    public async Task<PaginatedList<PurposeActiveList>> Handle(GetPurposeQuery request, CancellationToken cancellationToken)
+    {
+        if (request.authentication == null)
+        {
+            throw new UnauthorizedAccessException();
+        }
+
+        if (request.offset <= 0 || request.limit <= 0)
+        {
+            List<ValidationFailure> failures = new List<ValidationFailure> { };
+
+            if (request.offset <= 0)
+            {
+                failures.Add(new ValidationFailure("offset", "Offset must be greater than 0"));
+            }
+            if (request.limit <= 0)
+            {
+                failures.Add(new ValidationFailure("limit", "Limit must be greater than 0"));
+            }
+
+            throw new ValidationException(failures);
+        }
+
+        try
+        {
+            MapperConfiguration config = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<Consent_Purpose, PurposeActiveList>()
+             .ForMember(d => d.ExpiredDateTime, a => a.MapFrom(p => Calulate.ExpiredDateTime(p.KeepAliveData, p.CreateDate)))
+             .ForMember(d => d.CategoryID, a => a.MapFrom(s => s.PurposeCategoryId))
+             ;
+
+                cfg.CreateMap<string, Guid?>().ConvertUsing(value => !string.IsNullOrEmpty(value) ? Guid.Parse(value) : (Guid?)null);
+            });
+
+            Mapper mapper = new Mapper(config);
+
+            PaginatedList<PurposeActiveList> model =
+                await _context.DbSetConsentPurpose.Where(p => p.CompanyId == request.authentication.CompanyID && p.Status == Status.Active.ToString())
+                .ProjectTo<PurposeActiveList>(mapper.ConfigurationProvider).PaginatedListAsync(request.offset, request.limit);
+
+            return model;
+        }
+        catch (Exception ex)
+        {
+            throw new InternalServerException(ex.Message);
+        }
+    }
+}
